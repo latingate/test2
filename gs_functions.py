79662ed4fedf8363088408a5100607410bb7a7fa
galsarig <gal2016@latingate.com>
2021-07-24 20:38:03 +0300

    edits

--- gs_functions.py
+++ gs_functions.py
@@ -18,13 +18,11 @@ def check_python_version():
 def open_mongodb_connection():
     # conn = MongoClient('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
     # db = conn['tstdb']['tst2']
-    conn = MongoClient('mongodb+srv://latingate:mgal5313b@cluster0.oss2v.mongodb.net/test?authSource=admin&replicaSet=atlas-wgu8qa-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
-    db - conn['gal-tst-db']['gal-tst-1']
+    conn = MongoClient(
+        'mongodb+srv://latingate:mgal5313b@cluster0.oss2v.mongodb.net/test?authSource=admin&replicaSet=atlas-wgu8qa-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+    db = conn['gal-tst-db']['gal-tst-1']
 
     return db
 
 # User Functions
 # ================
-
-
-
