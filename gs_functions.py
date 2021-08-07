9737e8af21aa0cfbaa1659b24bc15b24446d4514
galsarig <gal2016@latingate.com>
2021-08-07 22:49:28 +0300

    edits

--- gs_functions.py
+++ gs_functions.py
@@ -24,7 +24,6 @@ def open_mongodb_connection():
     # cloud (MongoDB Atlas)
     conn = MongoClient("mongodb+srv://latingate:mgal5313b@cluster0.oss2v.mongodb.net/gal-tst-db?retryWrites=true&w=majority")
                      # 'mongodb+srv://latingate:mgal5313b@cluster0.oss2v.mongodb.net/gal-tst-db?authSource=admin&replicaSet=atlas-wgu8qa-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
-
     db = conn['gal-tst-db']['gal-tst-1']
     return db
 
