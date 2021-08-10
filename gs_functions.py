5fab40e2902a265c052d905477b0aafe61a83397
galsarig <gal2016@latingate.com>
2021-08-10 15:48:41 +0300

    edits

--- gs_functions.py
+++ gs_functions.py
@@ -18,13 +18,13 @@ def get_python_version():
 
 def open_mongodb_connection():
     # local
-    # conn = MongoClient('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
-    # db = conn['tstdb']['tst2']
+    conn = MongoClient('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
+    db = conn['tstdb']['tst2']
 
     # cloud (MongoDB Atlas)
-    conn = MongoClient("mongodb+srv://latingate:mgal5313b@cluster0.oss2v.mongodb.net/gal-tst-db?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
-                     # 'mongodb+srv://latingate:mgal5313b@cluster0.oss2v.mongodb.net/gal-tst-db?authSource=admin&replicaSet=atlas-wgu8qa-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
-    db = conn['gal-tst-db']['gal-tst-1']
+    # conn = MongoClient("mongodb+srv://latingate:mgal5313b@cluster0.oss2v.mongodb.net/gal-tst-db?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
+    #                  # 'mongodb+srv://latingate:mgal5313b@cluster0.oss2v.mongodb.net/gal-tst-db?authSource=admin&replicaSet=atlas-wgu8qa-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+    # db = conn['gal-tst-db']['gal-tst-1']
 
     return db
 
