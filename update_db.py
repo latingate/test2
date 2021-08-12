3e352bc3958359d3d47c0c5e9e1c32ca8e76ba7a
galsarig <gal2016@latingate.com>
2021-08-12 20:42:26 +0300

    edits

--- update_db.py
+++ update_db.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, request, render_template, request, session, url_for, jsonify
-from flask_dropzone import Dropzone
-from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 from pymongo import MongoClient
+from bson.objectid import ObjectId
+from dataclasses import dataclass
 import json
 import os
 
@@ -9,7 +9,15 @@ from gs_functions import *
 
 app = Flask(__name__)
 
-dropzone = Dropzone(app)
+
+@dataclass
+class User_db:
+    _id: str
+    first_name: str
+    last_name: str
+    initials: str
+    age: int
+    pics: dict
 
 
 @app.route("/")
@@ -19,21 +27,33 @@ def list_records():
     return render_template('list_records.html', cursor=cursor)
 
 
-@app.route("/edit_record/<id>")
-def edit_record(id):
+@app.route("/edit_record/<user_id>")
+def edit_record(user_id):
     db = open_mongodb_connection()
 
+    # filter_json = {
+    #     "name":
+    #         {"first": "Gal"}
+    # }
+
     filter_json = {
-        "_id": id
+        "_id": ObjectId(user_id)
     }
 
     sort_by = [('_id', 1)]
 
-    results = db.find(
+    results = db.find_one(
         filter=filter_json,
         sort=sort_by
     )
-    return render_template('edit_record.html', id=id)
+
+    user = User_db
+    user.first_name = results['name']['first']
+    user.last_name = results['name']['last']
+    user.initials = results['initials']
+    user.age = results['age']
+    # print(vars(user))
+    return render_template('edit_record.html', user_id=user_id, user=user)
 
 
 app.run(debug=True)
