379ba06eb62e81dfd9dc7d1ef4846ef63bab4100
latingate <gal2016@latingate.com>
2021-08-25 22:18:52 +0300

    various edits - home pc

--- mongoengine_test.py
+++ mongoengine_test.py
@@ -2,7 +2,7 @@ import mongoengine
 
 
 def mongoengine_connect():
-    mongoengine.register_connection(alias='alias_db', name='tstdb')
+    mongoengine.register_connection(alias='alias_db', name='tstdb', host='localhost', port=27017)
 
 
 mongoengine_connect()
