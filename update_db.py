f75b714de7c31cc65118103bb38cfe314d96a911
Gal Sarig <gal2016@latingate.com>
2021-09-13 16:27:01 +0300

    changes from macbook

--- update_db.py
+++ update_db.py
@@ -347,7 +347,7 @@ def add_db_user():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 # ajax - result-->
 # $(#div).html = render(get_results)
