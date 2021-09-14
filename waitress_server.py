88a89ae0d6dc1a6098c79513f3e4e2e621afda34
Gal Sarig <gal2016@latingate.com>
2021-09-14 14:00:51 +0300

    changes from IIS

--- waitress_server.py
+++ waitress_server.py
@@ -1,7 +1,7 @@
 from waitress import serve
 import update_db
 
-serve(update_db.app, host='0.0.0.0', port=80)
+serve(update_db.app, host='0.0.0.0', port=5000)
 
 # in order to use waitress server:
 # in the main app (ie update_db.app) replace this line:
