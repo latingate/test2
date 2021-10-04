1fe7a861161dbd175af3a3e409e4a827aad55f4e
latingate <gal2016@latingate.com>
2021-10-04 21:12:42 +0300

    various edits - home pc

---
+++ spf_check.py
@@ -0,0 +1,11 @@
+
+import spf
+
+print(spf.check(i='103.47.205.84', s='gal2016@latingate.com', h='smtp2go.com'))
+print(spf.check(i='103.47.205.84', s='gal2016@latingate.com', h='XXXXXsmtp2go.comXX'))
+print(spf.check(i='103.47.205.84', s='gal2016@latingate.com', h=''))
+print(spf.check(i='0.47.205.84', s='gal2016@latingate.com', h=''))
+
+print(spf.check(i='104.47.14.56', s='gals@hellmann.co.il', h='hellmann.co.il'))
+print(spf.check(i='0.47.14.56', s='gals@hellmann.co.il', h='hellmann.co.il'))
+print(spf.check2(i='0.47.14.56', s='gals@hellmann.co.il', h='hellmann.co.il'))
