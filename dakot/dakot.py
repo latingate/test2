3004f9e3d19a7fe5b3b86cd146acf92820f79dcc
Gal Sarig <gal2016@latingate.com>
2021-10-10 16:23:10 +0300

    changes from IIS

--- dakot/dakot.py
+++ dakot/dakot.py
@@ -4,7 +4,18 @@ from flask_paginate import Pagination, get_page_parameter
 
 from gs_functions import *
 
+app = Flask(__name__)
+
 
 @app.route('/podcast')
 def podcast_main():
     return render_template('podcast_main.html')
+
+
+if __name__ == '__main__':
+    # use the line below to run flask server directly (not recommended for production)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    # , use_reloader=False
+
+    # use the line below to run flask through waitress server, and than run waitress_server.py from pycharm or directly from command line using: python waitress_server.py
+    # app = Flask(__name__)
