96bdbefc93a1ac8dec4138ff6a72932cbfad9f00
galsarig <gal2016@latingate.com>
2022-01-20 18:06:49 +0200

    some changes - macbook

---
+++ tst_dropzone.py
@@ -0,0 +1,31 @@
+import os
+
+from flask import Flask, render_template, request
+from flask_dropzone import Dropzone
+
+# https://flask-dropzone.readthedocs.io/en/latest/configuration.html
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config.update(
+    UPLOADED_PATH= os.path.join(basedir,'uploads'),
+    DROPZONE_MAX_FILE_SIZE = 12,
+    DROPZONE_MAX_FILES = 2,
+    DROPZONE_INPUT_NAME = 'files',
+    # DROPZONE_ALLOWED_FILE_TYPE = 'audio',
+    DROPZONE_ALLOWED_FILE_CUSTOM = True,
+    DROPZONE_ALLOWED_FILE_TYPE = 'image/*, .mp3',
+    DROPZONE_TIMEOUT = 5*60*1000)
+
+dropzone = Dropzone(app)
+@app.route('/',methods=['POST','GET'])
+def upload():
+    if request.method == 'POST':
+        f = request.files.get('files')
+        print(f)
+        f.save(os.path.join(app.config['UPLOADED_PATH'],f.filename))
+    return render_template('tst_dropzone.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
