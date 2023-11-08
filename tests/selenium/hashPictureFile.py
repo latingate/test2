47ff01047c0d425a79d82733d2cd54970af87d7c
Gal Sarig <gal2016@latingate.com>
2023-11-08 15:52:44 +0200

    changes from office pc

--- tests/selenium/hashPictureFile.py
+++ tests/selenium/hashPictureFile.py
@@ -8,10 +8,14 @@ def compute_hash(filepath, hash_algorithm):
             hasher.update(chunk)
     return hasher.hexdigest()
 
-# image_filepath = 'tst.png'
-# md5_hash = compute_hash(image_filepath, 'md5')
-# sha256_hash = compute_hash(image_filepath, 'sha256')
-#
-# print(f"\nGenerate hash signature for a file - {image_filepath}")
-# print(f"MD5 hash of the image: {md5_hash}")
-# print(f"SHA-256 hash of the image: {sha256_hash}")
+
+if __name__ == '__main__':
+    # the following code is for testing purposes and will be run only if this script is not imported to another sciprt
+    image_filepath = 'tst.png'
+    md5_hash = compute_hash(image_filepath, 'md5')
+    sha256_hash = compute_hash(image_filepath, 'sha256')
+
+    print(f"\nGenerate hash signature for a file - {image_filepath}")
+    print(f"MD5 hash of the image: {md5_hash}")
+    print(f"SHA-256 hash of the image: {sha256_hash}")
+
