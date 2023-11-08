ff2748e7f59df8666f91eeae35561159da5144d5
Gal Sarig <gal2016@latingate.com>
2023-11-08 14:43:43 +0200

    changes from office pc

--- tests/selenium/main.py
+++ tests/selenium/main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -6,9 +7,11 @@ from time import sleep
 
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=options)
-# driver = webdriver.Firefox()
+
 
 LOGIN_URL = 'https://www.facebook.com/login.php'
+# LOGIN_URL = 'https://www.facebook.com/FolkMusicIsrael/posts/pfbid02QqJZ1xCueK69pA5DxknnZdWRT1HqYmzcSBj14G9suQZjVr9uxnfa48F811Ucn1bYl'
+
 
 class FacebookLogin():
     def __init__(self, email, password, browser='chrome'):
@@ -20,54 +23,62 @@ class FacebookLogin():
             # self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
             # self.driver = webdriver.Chrome(ChromeDriverManager().install())
             self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-        elif browser == 'firefox':
-            # Set it to Firefox
-            self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
         self.driver.get(LOGIN_URL)
-        sleep(1) # Wait for some time to load
+        sleep(1)
 
 
     def login(self):
-        # email_element = self.driver.find_element_by_id('email')
-        # email_element = self.driver.find_element("email", "q")
-        # email_element = self.driver.find_element(self.driver.find_element.By.ID, "email")
         email_element = self.driver.find_element('id', "email")
         email_element.send_keys(self.email)  # Give keyboard input
 
-        # password_element = self.driver.find_element_by_id('pass')
-        # password_element.send_keys(self.password)  # Give password as input too
         password_element = self.driver.find_element('id', "pass")
         password_element.send_keys(self.password)  # Give keyboard input
 
-        # login_button = self.driver.find_element_by_id('loginbutton')
+        # self.driver.get_screenshot_as_file("screenshotLogin.png")
         login_button = self.driver.find_element('id', "loginbutton")
-        print('login_button',login_button)
+        sleep(1)
         login_button.click()  # Send mouse click
+        sleep(2)
 
-        sleep(2)  # Wait for 2 seconds for the page to show up
+
+    def screenCapture(self, postURL,reportID):
+        # postURL = 'https://www.facebook.com/FolkMusicIsrael/posts/pfbid02QqJZ1xCueK69pA5DxknnZdWRT1HqYmzcSBj14G9suQZjVr9uxnfa48F811Ucn1bYl'
+        self.driver.get(postURL)
+        self.driver.get_screenshot_as_file("screenshotPost_0_" + reportID + ".png")
+        sleep(1)
+        self.driver.get_screenshot_as_file("screenshotPost_1_" + reportID + ".png")
+        sleep(2)
+        self.driver.get(postURL)
+        self.driver.get_screenshot_as_file("screenshotPost_2_" + reportID + ".png")
+        sleep(1)
+        self.driver.get_screenshot_as_file("screenshotPost_3_" + reportID + ".png")
 
 
-    def screenCapture(self):
+    def dismissAlert(self):
+        alert = Alert(self.driver)
+        print(alert.text)
+        alert.dismiss()
 
 
 if __name__ == '__main__':
-    print('trying to log in')
-    # Enter your login credentials here
+    postURL = 'https://www.facebook.com/rogerwaters/posts/pfbid02HRtXq4b5J1Jkx9Am3BvYprbhZESAMAsgafbD2SoDRCqNPCWoTYKBb98AxF5zV3yrl'
+    reportID = '28154'
     try:
-        fb_login = FacebookLogin(email='henriettamaragoza', password='fgal5313k', browser='chrome')
+        # fb_login = FacebookLogin(email='henriettamaragoza', password='fgal5313k', browser='chrome')
+        fb_login = FacebookLogin(email='galtmp@gmail.com', password='fgal5313k', browser='chrome')
         fb_login.login()
+        fb_login.screenCapture(postURL,reportID)
     except Exception as error:
         print("An exception occurred:", error, type(error).__name__)  # An exception occurred: ZeroDivisionError
         exit()
-    print('logged in')
 
 
 
-print('starting screen capture')
-# driver.get('https://reva5.co.il')
-driver.get('https://www.facebook.com/FolkMusicIsrael/posts/pfbid02QqJZ1xCueK69pA5DxknnZdWRT1HqYmzcSBj14G9suQZjVr9uxnfa48F811Ucn1bYl')
-# driver.get('https://www.instagram.com/reel/CzV9RnRKEVO/?utm_source=ig_web_copy_link')
-sleep(2)
-driver.get_screenshot_as_file("screenshot.png")
-driver.quit()
-print("end screen capture")
+# print('starting screen capture')
+# # driver.get('https://reva5.co.il')
+# driver.get('https://www.facebook.com/FolkMusicIsrael/posts/pfbid02QqJZ1xCueK69pA5DxknnZdWRT1HqYmzcSBj14G9suQZjVr9uxnfa48F811Ucn1bYl')
+# # driver.get('https://www.instagram.com/reel/CzV9RnRKEVO/?utm_source=ig_web_copy_link')
+# sleep(2)
+# driver.get_screenshot_as_file("screenshot.png")
+# driver.quit()
+# print("end screen capture")
